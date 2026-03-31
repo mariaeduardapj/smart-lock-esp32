af5fb7f42f71a16c7ba81a2ebdec3bcab1a67ede
Maria Eduarda P. Jesus <mariaeduardapereiraje@gmail.com>
2026-03-31 11:07:17 -0300

    fix: corrects version details

--- src/smart-lock.cpp
+++ src/smart-lock.cpp
@@ -1,15 +1,17 @@
-const String SYSTEM_VERSION = "v1.1.0";
-
 #include <Arduino.h>
 
+#define SYSTEM_NAME "Smart Lock"
+#define SYSTEM_VERSION "v1.0.0"
 #define ButtonPin 4
 #define LockPin 5
 // Identifies the microcontroller's inputs and outputs.
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Smart Lock desenvolved by Maria Eduarda P. Jesus");
-  Serial.println(SYSTEM_VERSION);
+  Serial.println("=====================================");
+  Serial.printf("%s %s\n", SYSTEM_NAME, SYSTEM_VERSION);
+  Serial.println("Developed by Maria Eduarda P. Jesus");
+  Serial.println("=====================================");
     
   pinMode(ButtonPin, INPUT_PULLDOWN);
   pinMode(LockPin, OUTPUT);
